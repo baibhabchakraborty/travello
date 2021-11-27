45d53603680e07a330be88311ad98ed38183cf42
Baibhab Chakraborty <baishakhi.baibhab@gmail.com>
2021-11-27 12:56:11 +0530

    final commit

--- travello/views.py
+++ travello/views.py
@@ -5,27 +5,6 @@ from .models import Destination
 
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = 'City that never sleeps'
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 800
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.name = 'Hyderabad'
-    dest2.desc = 'Biryani first!'
-    dest2.img = 'destination_2.jpg'
-    dest2.price = 600
-    dest2.offer = False
-
-    dest3 = Destination()
-    dest3.name = 'Bengaluru'
-    dest3.desc = 'Namma Bengaluru'
-    dest3.img = 'destination_3.jpg'
-    dest3.price = 400
-    dest3.offer = True
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
 
     return render(request,'index.html', {'dests': dests})
